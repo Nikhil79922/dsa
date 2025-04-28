2e4d0dd79aa3772e02c415dad743f01ba4197f49
Nikhil79922 <ns94301918@gmail.com>
2025-04-28 16:38:35 +0530

    leetcode question 461 done

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,17 +1,1 @@
-var productExceptSelf = function(nums) {
-//     let n=nums.length;
-//     let result=[];
-//     for(let i=0;i<n;i++){
-//         var product=1;
-//        for(let j=0;j<n;j++){
-        
-//              if(i==j){
-//                 continue;
-//              }
-//             product=product*nums[j]
-    
-//     }
-//     result.push(product)
-//     }
-//     return result
-//     };
+            console.log("outPut_arr",output_arr)
