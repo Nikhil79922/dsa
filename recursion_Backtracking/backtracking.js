043fd9c7cb5e550f3c864e04683cf0af879ef5b6
Nikhil79922 <ns94301918@gmail.com>
2026-02-02 23:18:52 +0530

    Add the N Queue problem solution

--- recursion_Backtracking/backtracking.js
+++ recursion_Backtracking/backtracking.js
@@ -11,3 +11,49 @@ function subset(arr,ans,i){
 }
 
 console.log("subset===>",subset([1,2,3],[],0))
+
+
+//N -queue 
+var solveNQueens = function (n) {
+    let board = Array.from({ length: n }, () => Array(n).fill('.'));
+       let ans = [];
+       let row = 0;
+   
+       function quene(board, row) {
+           if (row == n) {
+       ans.push(board.map(r => r.join('')));
+               return;
+           }
+           for (let j = 0; j<n; j++) {
+               if (isSafe(board, row, j, n)) {
+                   board[row][j] = 'Q';
+                   quene(board, row + 1);
+                   board[row][j] = '.';
+               }
+   
+           }
+       }
+       quene(board,row);
+       return ans;
+   };
+   const isSafe = (board, row , column, n) => {
+       //Column vertical check
+       for (let i = 0; i < row; i++) {
+           if (board[i][column] == 'Q') {
+               return false;
+           }
+       }
+       //Left diagonal check
+       for (let r1 = row - 1, c1 = column - 1; r1 >= 0 && c1 >= 0; r1--, c1--) {
+           if (board[r1][c1] == 'Q') {
+               return false;
+           }
+       }
+       //Right diagonal check
+       for (let r1 = row - 1, c1 = column + 1; r1 >= 0 && c1 < n; r1--, c1++) {
+           if (board[r1][c1] == 'Q') {
+               return false;
+           }
+       }
+       return true;
+   }
