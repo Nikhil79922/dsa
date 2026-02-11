4b6b812d8ce498dc421893c4d183f87a9d993ac9
Nikhil79922 <ns94301918@gmail.com>
2026-02-11 23:31:51 +0530

    Added the function linkde List

--- Linked_List/linkedList_practices.js
+++ Linked_List/linkedList_practices.js
@@ -123,3 +123,31 @@ list.print();
 console.log("Reverse Method ::")
 list.reverse()
 list.print();
+
+
+
+//Function 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let current = head;
+    let prev = null;
+
+    while (current !== null) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+};
