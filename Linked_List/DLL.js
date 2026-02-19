c891c701a5e0a02cc83cb8f43f6219d25ed17fb3
Nikhil79922 <ns94301918@gmail.com>
2026-02-19 20:59:54 +0530

    Added the DLL

--- Linked_List/DLL.js
+++ Linked_List/DLL.js
@@ -7,8 +7,9 @@ class DLL {
   }
   class DLLOPS{
     constructor(){
-      this.head=isNull;
+      this.head=null;
     }
+    //Head
     insertHead(val){
      let newNode=new DLL(val)
      if(!this.head) {
@@ -19,6 +20,7 @@ class DLL {
       this.head.prev=newNode;
       this.head=newNode;
     }
+    //Tail
     insertTail(val){
       let newNode=new DLL(val)
       if(!this.head) {
@@ -32,25 +34,88 @@ class DLL {
      newNode.prev=current
      current.next=newNode;
     }
+
+    // Delete 
     deleteNode(val){
       if(!this.head) {
         return;
       }
       if(this.head.val == val){
         this.head=this.head.next;
+        if(!this.head) {
+          return;
+        }
+        this.head.prev=null;
+        return;
       }
       let current= this.head;
-      let prev=null;
       while(current !== null){
         if(current.val==val){
           let next= current.next;
-         prev.next=next
-         if(next!==null) next.prev=current.prev;
+         if(next==null){
+current.prev.next=next
+         }else{
+          next.prev=current.prev;
+          current.prev.next=next
+         }
+         break;
         }
-        prev=current;
         current=current.next;
       }
     }
+
+    // Print Forward
+    printForward(){
+      if(!this.head) {
+        return;
+      }
+      let current=this.head;
+      while(current !== null){
+        console.log(current.val);
+        current=current.next;
+      }
+    }
+
+    reverse(){
+      if(!this.head) {
+        return;
+      }
+
+    let current=this.head;
+    let previous=null;
+    while(current !== null ){
+      let next= current.next;
+      current.next=previous;
+      previous=current;
+      previous.prev=null
+      current=next;
+    }
+    while(previous !== null){
+      console.log(previous.val);
+      previous=previous.next;
+    }
+    }
   }
 
-  
+const Doubly=new DLLOPS()
+Doubly.insertHead(10)
+Doubly.insertHead(20)
+Doubly.insertHead(30)
+Doubly.insertHead(40)
+
+Doubly.insertTail(50)
+Doubly.insertTail(60)
+Doubly.insertTail(70)
+
+Doubly.deleteNode(10)
+Doubly.deleteNode(60)
+
+console.log('Print Forward')
+Doubly.printForward()
+
+console.log('Print Backward')
+Doubly.reverse()
+
+
+
+
