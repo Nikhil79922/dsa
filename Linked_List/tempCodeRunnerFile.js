e76097566c9e37213a5fb9128a6bf0df7845a56a
Nikhil79922 <ns94301918@gmail.com>
2026-02-19 20:59:54 +0530

    Added the DLL

--- Linked_List/tempCodeRunnerFile.js
+++ Linked_List/tempCodeRunnerFile.js
@@ -1,1 +1,23 @@
-current.next
+   reverse(){
+      if(!this.head) {
+        return;
+      }
+      let current=this.head;
+      let temp =null;
+      while(current!==null){
+        if(current.next==null){
+          this.head=current
+        }
+        temp=current.next;
+        current.next=current.prev;
+        current.prev=temp;
+
+        //Now forward
+        current=current.prev;
+      } 
+      let curr=this.head
+      while(curr !== null){
+        console.log(curr.val);
+        curr=curr.next;
+      }
+    }
