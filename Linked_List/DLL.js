7e3df1b856c05c61a9de23b44761be5ed2927760
Nikhil79922 <ns94301918@gmail.com>
2026-02-19 20:59:29 +0530

    Added the DLL

--- Linked_List/DLL.js
+++ Linked_List/DLL.js
@@ -1,51 +1,56 @@
-class DLL{
+class DLL {
     constructor(val){
-        this.val=val;
-        this.next=null;
-        this.prev=null;
+      this.val=val;
+      this.prev=null;
+      this.next=null;
     }
-}
-
-class DoublyLinkedListOps{
+  }
+  class DLLOPS{
     constructor(){
-        this.head=null;
+      this.head=isNull;
     }
-
-    InsertAtHead(val){
-    let newDLLNode=new DLL(val)
-        if(!this.head) this.head=newDLLNode 
-        newDLLNode.next=this.head    // Move Backward
-        this.head.prev=newDLLNode   // Move forward
-        this.head = newDLLNode     //Move head 
-
-        return this.head;
+    insertHead(val){
+     let newNode=new DLL(val)
+     if(!this.head) {
+      this.head=newNode;
+      return;
     }
-
-    InsertAtTail(val){
-        let newDLLNode=new DLL(val)
-        if(!this.head) this.head=newDLLNode 
-        let current=this.head;
-        while(current.next !== null){
-            current=current.next;
-        }
-        current.next=newDLLNode
-        newDLLNode.prev=current;
+      newNode.next=this.head;
+      this.head.prev=newNode;
+      this.head=newNode;
     }
-
+    insertTail(val){
+      let newNode=new DLL(val)
+      if(!this.head) {
+        this.head=newNode;
+        return;
+      }
+        let current =this.head;
+     while(current.next !== null){
+      current=current.next;
+     }
+     newNode.prev=current
+     current.next=newNode;
+    }
     deleteNode(val){
-        if(!this.head) return ;
-        let current= this.head;
-        if(current.val == val) return null;
-        while(current !== null){
-            if(current.val==val){
-              
-            }
+      if(!this.head) {
+        return;
+      }
+      if(this.head.val == val){
+        this.head=this.head.next;
+      }
+      let current= this.head;
+      let prev=null;
+      while(current !== null){
+        if(current.val==val){
+          let next= current.next;
+         prev.next=next
+         if(next!==null) next.prev=current.prev;
         }
+        prev=current;
+        current=current.next;
+      }
     }
-}
+  }
 
-let DL=new DoublyLinkedListOps()
-DL.InsertAtHead(10)
-DL.InsertAtHead(20)
-DL.InsertAtHead(30)
-console.log(DL.InsertAtHead(40))
+  
