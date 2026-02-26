11298ad4025c59d0988469ea5856f6a7b72a3a2f
Nikhil_Singh.dev <139366599+Nikhil79922@users.noreply.github.com>
2026-02-26 05:39:39 +0000

    Added the class questions

--- OOP/classes.js
+++ OOP/classes.js
@@ -24,5 +24,33 @@ this.speed=speed
 }
 
 let c1=new car('BWM',200);
+console.log("Car ===>",c1)
 c1.move();
 c1.fuel()
+
+
+
+
+//Problem statement 
+class BankAccount{
+    #balance=0;
+    constructor(balance){
+this.desposit(balance);
+    }
+
+    desposit(amount){
+        if(amount<=0) console.warn(`Don't Accept Zero or Less than Zero value.`)
+         this.#balance+=amount;
+    }
+
+    getBalance(){
+        return this.#balance;
+    }
+}
+
+const BA= new BankAccount(10);
+BA.desposit(20)
+BA.desposit(30)
+BA.desposit(40)
+let totalSaving = BA.getBalance();
+console.log("TotalSaving ===>",totalSaving)
