0e29f6eb3c0369b53569189421852e186a5936ba
Nikhil79922 <ns94301918@gmail.com>
2026-03-02 23:23:29 +0530

    Added the LRU cache implementation

--- Linked_List/LRU_caching.js
+++ Linked_List/LRU_caching.js
@@ -1,46 +1,55 @@
 /**
  * @param {number} capacity
  */
+
 class Node {
     constructor(key, value) {
         this.key = key;
-        this.value = value || null;
+        this.value = value;
         this.prev = null;
         this.next = null;
     }
 }
+
 var LRUCache = function (capacity) {
-    this.limit = capacity
+    this.limit = capacity;
     this.head = new Node(-1, -1);
     this.tail = new Node(-1, -1);
-
     this.head.next = this.tail;
     this.tail.prev = this.head;
 
     this.map = new Map();
+
 };
 
-LRUCache.prototype.add = function (node) {
-    let nextNode = this.head.next;
-
-    this.head.next = node;
-    node.prev = this.head;
-
-    node.next = nextNode;
-    nextNode.prev = node;
+LRUCache.prototype.add = function (newNode) {
+    let next = this.head.next;
+    this.head.next = newNode;
+    newNode.next = next;
+    newNode.prev = this.head
+    next.prev = newNode;
 }
 
-LRUCache.prototype.delete = function (node) {
-    node.prev.next = node.next;
-    node.next.prev = node.prev;
+LRUCache.prototype.delete = function (newNode) {
+    let prev = newNode.prev;
+    prev.next = newNode.next;
+    newNode.next.prev = prev;
 }
+
 
 /** 
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function (key) {
+    if (!this.map.has(key)) return -1;
 
+    let node = this.map.get(key);
+
+    this.delete(node);
+    this.add(node);
+
+    return node.value;
 };
 
 /** 
@@ -49,18 +58,16 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
-if(this.map.has(key)){
-    let node = this.map.key(key);
-        this.delete(newNode);
-        this.map.delete(key)
-}
-
-if(this.map.size == this.limit){
-    this.delete(this.tail.prev);
-    this.map.delete(this.tail.prev.key)
-}
-
-
+    if (this.map.has(key)) {
+        let existing = this.map.get(key);
+        this.delete(existing);
+        this.map.delete(key);
+    }
+    else if (this.map.size === this.limit) {
+        let lru = this.tail.prev;
+        this.delete(lru);
+        this.map.delete(lru.key);
+    }
     let newNode = new Node(key, value)
     this.map.set(key, newNode);
     this.add(newNode);
