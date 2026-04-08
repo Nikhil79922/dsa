d6194c5861b6329dfa09cc3cc4507a9db93786ca
Nikhil79922 <ns94301918@gmail.com>
2026-04-09 01:08:06 +0530

    added the quick sort code

--- recursion_Backtracking/recursion.js
+++ recursion_Backtracking/recursion.js
@@ -238,3 +238,43 @@ return arr;
 }
 
 mergeSortPractice([1,3,5,7,9,3,2,5,7,1,1,1,4,7,9])
+
+
+const quickSort = function(arr) {
+
+   function quick(arr , low , high){
+let pivot = arr[low];
+let i =low;
+let j= high;
+
+while(i<j){
+   while(i<=j && arr[i] >= pivot){
+      i++;
+   }
+   while(i<=j && arr[j] <= pivot){
+      j--;
+   }
+   if(i<j){
+[arr[i] , arr[j]] = [arr[j] , arr[i]];
+   }
+}
+
+[arr[low] , arr[j]] = [arr[j] , arr[low]];
+return j;
+
+   }
+
+   function sort(arr, low , high){
+   if(low>=high) {
+      return
+   }
+  let partitionIndex = quick(arr , low , high);
+  sort(arr , low , partitionIndex-1)
+  sort(arr , partitionIndex+1 , high)
+   }
+   sort(arr , 0 , arr.length-1);
+   console.log("Quick Sort",arr);
+
+}
+
+quickSort([1,3,5,7,9,3,2,5,7,1,1,1,4,7,9])
