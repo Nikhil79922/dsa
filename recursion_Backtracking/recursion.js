3d5c4b79ca5a7614d4e345a4c9bdaddb20f7af08
Nikhil79922 <ns94301918@gmail.com>
2026-04-13 21:59:09 +0530

    formating

--- recursion_Backtracking/recursion.js
+++ recursion_Backtracking/recursion.js
@@ -202,7 +202,6 @@ function mergeSortPractice (arr){
       let mid= Math.floor((low + high)/2);
       mergeSort(arr, low , mid);
       mergeSort(arr , mid+1 , high);
-         
    }
 
    function merge(arr , low , mid , high){
