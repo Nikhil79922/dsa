40d730d850f24a6eed10f22be2fdfe71c2102587
Nikhil79922 <ns94301918@gmail.com>
2026-05-04 23:35:27 +0530

    Added the Product of Array Except Self

--- Array/practices.js
+++ Array/practices.js
@@ -891,3 +891,23 @@ let output = Infinity;
     return output
 };
 
+
+//238
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    let n = nums.length;
+    let prefix= new Array(n).fill(1);
+    let suffix=1;
+    for(let i = 1 ; i<n ; i++ ){
+      prefix[i] = prefix[i-1] * nums[i-1];
+    }
+  
+  for(let i=n-1;i>=0;i--){
+      prefix[i] *= suffix ;
+      suffix *= nums[i];
+  }
+  return prefix;
+  };
