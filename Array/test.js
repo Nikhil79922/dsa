a703f3e7d4686e318fecdd7577180a24b5e01dd6
Nikhil79922 <ns94301918@gmail.com>
2026-05-06 08:48:06 +0530

    added the longest palindrome problem solution

--- Array/test.js
+++ Array/test.js
@@ -349,3 +349,31 @@ var isValid=(nums , n , C , mid)=>{
 }
 
 maxDistance([5,4,3,2,1,1000000000],2)
+
+
+
+//LongestPalindrome 
+var longestPalindrome = function(s) {
+    let start = 0, end = 0;
+
+    function expand(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let len1 = expand(i, i);       // odd
+        let len2 = expand(i, i + 1);   // even
+        let len = Math.max(len1, len2);
+
+        if (len > end - start) {
+            start = i - Math.floor((len - 1) / 2);
+            end = i + Math.floor(len / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);
+};
