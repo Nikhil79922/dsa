31c131564393200ffc79f749e22c755e2df96b3f
Nikhil79922 <ns94301918@gmail.com>
2026-05-31 21:40:06 +0530

    Added the detect cycle start index  LL problem

--- Linked_List/practices.js
+++ Linked_List/practices.js
@@ -160,3 +160,34 @@ var hasCycle = function(head) {
   }
   return false;
   };
+
+
+  /**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) {
+            let pointer = head;
+            while (slow !== pointer) {
+                pointer = pointer.next;
+                slow = slow.next;
+            }
+            return pointer;
+        }
+    }
+    return null;
+};
