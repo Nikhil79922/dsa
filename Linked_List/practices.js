2ad2c564bf5ab29f0e034a5a417c26f9eeaf5fe4
Nikhil79922 <ns94301918@gmail.com>
2026-05-31 18:54:20 +0530

    Added the merge sort problem

--- Linked_List/practices.js
+++ Linked_List/practices.js
@@ -44,3 +44,57 @@ var middleNode = function (head) {
     }
     return slow
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+    let dummy = null;
+    let dummyCurr = null
+    var setDummy = function (val) {
+        console.log(val)
+        let node = {
+            val,
+            next: null
+        }
+        if (dummy == null) {
+            dummy = node
+            dummyCurr = dummy
+        } else {
+            dummyCurr.next = node
+            dummyCurr = dummyCurr.next
+        }
+    }
+    while (list1 != null || list2 != null) {
+        if (list1 == null) {
+            setDummy(list2.val)
+            list2 = list2.next;
+            continue;
+        }
+        if (list2 == null) {
+            setDummy(list1.val)
+            list1 = list1.next;
+              continue;
+        }
+
+        if (list1.val > list2.val) {
+            setDummy(list2.val)
+            list2 = list2.next;
+        } else {
+            setDummy(list1.val)
+            list1 = list1.next;
+        }
+    }
+    return dummy
+};
