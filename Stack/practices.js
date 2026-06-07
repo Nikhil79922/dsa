5189691689c46dcb428a3ecf282a88b939d98efb
Nikhil79922 <ns94301918@gmail.com>
2026-06-07 21:40:09 +0530

    Added the Valid Parentheses  problem solution

--- Stack/practices.js
+++ Stack/practices.js
@@ -75,3 +75,40 @@ var nextGreaterElement = function (nums1, nums2) {
 
     return nums1.map(num => map.get(num));
 };
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack.push(s[i]);
+        }
+        else if (s[i] == ')') {
+            if (stack[stack.length - 1] !== '(') {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+        else if (s[i] == '}') {
+            if (stack[stack.length - 1] !== '{') {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+        else if (s[i] == ']') {
+            if (stack[stack.length - 1] !== '[') {
+                return false
+            } else {
+                stack.pop()
+            }
+        }
+
+
+    }
+  return stack.length == 0;
+};
