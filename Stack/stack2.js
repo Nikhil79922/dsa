e5b8bb6d21d4b10e3cc440669a93d86826a39b14
Nikhil79922 <ns94301918@gmail.com>
2026-06-09 07:51:15 +0530

    Added the 134. Gas Station problem

--- Stack/stack2.js
+++ Stack/stack2.js
@@ -354,3 +354,31 @@ var MinStack = function() {
    * var param_3 = obj.top()
    * var param_4 = obj.getMin()
    */
+
+
+  /**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+  let totalGas = 0;
+  let totalCost= 0;
+  
+  for(let i =0 ; i< gas.length ; i++){
+      totalGas += gas[i];
+      totalCost += cost[i];
+  }
+  if(totalCost > totalGas) return -1;
+  
+  let curr = 0 ;
+      let start= 0 ;
+  for(let i = 0 ; i<gas.length ; i++){
+      curr += gas[i] - cost[i];
+      if(curr < 0 ){
+          start=i+1;
+  curr = 0 ;
+      } 
+  }
+  return start
+  };
