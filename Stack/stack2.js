c8b9a5568894f00edc1fa1e2887e07c4b36a16f2
Nikhil79922 <ns94301918@gmail.com>
2026-06-11 07:58:13 +0530

    Added the celebrity problem from stack

--- Stack/stack2.js
+++ Stack/stack2.js
@@ -1,96 +1,96 @@
 class stack {
-    stk = [];
+  stk = [];
 
-    push(e) {
-       return  this.stk.push(e)
-    }
-    pop(){
-      return this.stk.pop();
-    }
-    peek(){
-        if(this.size()) return this.stk[this.size()-1];
-        return null;
-    }
-    size(){
-        return this.stk.length;
-    }
-    isEmpty(){
-        if(this.size()) return false;
-        return true;
-    }
+  push(e) {
+    return this.stk.push(e)
+  }
+  pop() {
+    return this.stk.pop();
+  }
+  peek() {
+    if (this.size()) return this.stk[this.size() - 1];
+    return null;
+  }
+  size() {
+    return this.stk.length;
+  }
+  isEmpty() {
+    if (this.size()) return false;
+    return true;
+  }
 }
 
 
-let sta= new stack();
+let sta = new stack();
 sta.push(10)
 sta.push(20)
 sta.push(30)
 sta.push(40)
 sta.pop()
 console.log(sta.stk)
-  class Valid_Parentheses {
-    constructor(para) {
-      this.para = para;
-    }
-    stk = [];
-    peek() {
-      if (this.size()!==0) return this.stk[this.size()-1];
-      return null;
-    }
-size(){
-  return this.stk.length;
-}
-    valid() {
-      let arr = this.para.split('');
-      for (let i = 0; i < arr.length; i++) {
-        if (arr[i] =='{' || arr[i] == '(' || arr[i] == '[') {
-          this.stk.push(arr[i])
-        } else {
-          if (this.size()) {
-          
-            if(((this.peek() == '(' )&& (arr[i] == ')' ))|| ((this.peek() == '[' )&& (arr[i] == ']' )) || ((this.peek() == '{' )&& (arr[i] == '}' )) ){
-              this.stk.pop()
-            }else{
-              return false
-            }
+class Valid_Parentheses {
+  constructor(para) {
+    this.para = para;
+  }
+  stk = [];
+  peek() {
+    if (this.size() !== 0) return this.stk[this.size() - 1];
+    return null;
+  }
+  size() {
+    return this.stk.length;
+  }
+  valid() {
+    let arr = this.para.split('');
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] == '{' || arr[i] == '(' || arr[i] == '[') {
+        this.stk.push(arr[i])
+      } else {
+        if (this.size()) {
+
+          if (((this.peek() == '(') && (arr[i] == ')')) || ((this.peek() == '[') && (arr[i] == ']')) || ((this.peek() == '{') && (arr[i] == '}'))) {
+            this.stk.pop()
           } else {
             return false
           }
+        } else {
+          return false
         }
-
-
       }
-      return this.size() === 0; 
+
+
     }
+    return this.size() === 0;
   }
+}
 
-   class reverse{
-    constructor(str){
-      this.str=str;
+class reverse {
+  constructor(str) {
+    this.str = str;
+  }
+  stk = []
+  rev() {
+    let arr = this.str.split('');
+    for (let i = arr.length - 1; i >= 0; i--) {
+      this.stk.push(arr[i]);
     }
-    stk=[]
-    rev(){
-      let arr =this.str.split('');
-      for(let i=arr.length-1 ; i>=0 ; i--){
-         this.stk.push(arr[i]);
-      }
-      return this.stk.join('');
-    }  
+    return this.stk.join('');
   }
+}
 
 
-  class nextGreater{
-  constructor(arr){
-    this.arr=arr;
+class nextGreater {
+  constructor(arr) {
+    this.arr = arr;
   }
-  NG(){
-    let stack= [];
+  NG() {
+    let stack = [];
     let result = new Array(this.arr.length).fill(-1);
     stack.push(i);
-    for(let i=0; i<this.arr.length; i++){
-      while(stack.length > 0 && this.arr[i] > this.arr[stack[stack.length -1]]){
+    for (let i = 0; i < this.arr.length; i++) {
+      while (stack.length > 0 && this.arr[i] > this.arr[stack[stack.length - 1]]) {
         let index = stack.pop();
-        result[index]=this.arr[i];
+        result[index] = this.arr[i];
       }
 
     }
@@ -111,113 +111,113 @@ var maxSumMinProduct = function (nums) {
   let stack2 = [];
 
   for (let i = 0; i < n; i++) {
-      prefix[i] = i === 0 ? nums[i] : prefix[i - 1] + nums[i];
-      while (stack.length && nums[stack[stack.length - 1]] >= nums[i]) {
-          stack.pop();
-      }
-      leftMin[i] = stack.length ? stack[stack.length - 1] : -1;
-      stack.push(i);
+    prefix[i] = i === 0 ? nums[i] : prefix[i - 1] + nums[i];
+    while (stack.length && nums[stack[stack.length - 1]] >= nums[i]) {
+      stack.pop();
+    }
+    leftMin[i] = stack.length ? stack[stack.length - 1] : -1;
+    stack.push(i);
 
-      while (stack2.length && nums[stack2[stack2.length - 1]] > nums[i]) {
-          let idx = stack2.pop();
-          rightMin[idx] = i
-      }
-      stack2.push(i)
+    while (stack2.length && nums[stack2[stack2.length - 1]] > nums[i]) {
+      let idx = stack2.pop();
+      rightMin[idx] = i
+    }
+    stack2.push(i)
   }
 
-  let max = 0n; 
+  let max = 0n;
 
   for (let i = 0; i < n; i++) {
-      let left = leftMin[i] + 1;
-      let right = rightMin[i] - 1;
+    let left = leftMin[i] + 1;
+    let right = rightMin[i] - 1;
 
-      let total = BigInt(prefix[right]) - BigInt(left > 0 ? prefix[left - 1] : 0);
-      let curr = total * BigInt(nums[i]);
+    let total = BigInt(prefix[right]) - BigInt(left > 0 ? prefix[left - 1] : 0);
+    let curr = total * BigInt(nums[i]);
 
-      if (curr > max) max = curr;
+    if (curr > max) max = curr;
   }
   return Number(max % 1000000007n);
 };
 
 
-var canCompleteCircuit = function(gas, cost) {
-  let totalGas= gas[0];
-  let totalCost= cost[0];
-  
-  for(let i = 1 ;i<gas.length; i++){
-      totalGas += gas[i];
-      totalCost += cost[i];
+var canCompleteCircuit = function (gas, cost) {
+  let totalGas = gas[0];
+  let totalCost = cost[0];
+
+  for (let i = 1; i < gas.length; i++) {
+    totalGas += gas[i];
+    totalCost += cost[i];
   }
-  if(totalCost > totalGas){
-      return -1;
+  if (totalCost > totalGas) {
+    return -1;
   }
-  let start= 0 ;
-  let curr= 0;
-  
-  for(let i = 0 ;i<cost.length; i++){
-      curr += gas[i] - cost[i]
-      if(curr < 0){
-          start = i+ 1;
-          curr= 0
-      }
+  let start = 0;
+  let curr = 0;
+
+  for (let i = 0; i < cost.length; i++) {
+    curr += gas[i] - cost[i]
+    if (curr < 0) {
+      start = i + 1;
+      curr = 0
+    }
   }
-  return start 
-  };
+  return start
+};
 
 
-  /**
- * @param {number[]} heights
- * @return {number}
- */
-var largestRectangleArea = function(heights) {
-  let n =heights.length;
-  let left=new Array(n).fill(-1);
-  let stack1 =[]
+/**
+* @param {number[]} heights
+* @return {number}
+*/
+var largestRectangleArea = function (heights) {
+  let n = heights.length;
+  let left = new Array(n).fill(-1);
+  let stack1 = []
   let right = new Array(n).fill(n);
-  let stack2=[];
- 
- for(let i=0 ; i<heights.length; i++){
- while(stack1.length && heights[stack1[stack1.length -1]] > heights[i]){
-     stack1.pop();
- }
- left[i]= stack1.length ? stack1[stack1.length -1] : -1;
- stack1.push(i);
- 
- while(stack2.length && heights[stack2[stack2.length -1]] > heights[i]){
-     const insert = stack2.pop();
-     right[insert]= i
- }
- stack2.push(i);
- }
- let maxArea= 0 ;
- for(let i= 0 ; i<heights.length ; i++){
-     let l= left[i] + 1;
-     let r= right[i];
-     let area= heights[i] * (r-l)
-     maxArea= Math.max(maxArea , area);
- }
- return maxArea;
- };
+  let stack2 = [];
 
- var stockspan = function(stock) {
-  let n= stock.length;
+  for (let i = 0; i < heights.length; i++) {
+    while (stack1.length && heights[stack1[stack1.length - 1]] > heights[i]) {
+      stack1.pop();
+    }
+    left[i] = stack1.length ? stack1[stack1.length - 1] : -1;
+    stack1.push(i);
+
+    while (stack2.length && heights[stack2[stack2.length - 1]] > heights[i]) {
+      const insert = stack2.pop();
+      right[insert] = i
+    }
+    stack2.push(i);
+  }
+  let maxArea = 0;
+  for (let i = 0; i < heights.length; i++) {
+    let l = left[i] + 1;
+    let r = right[i];
+    let area = heights[i] * (r - l)
+    maxArea = Math.max(maxArea, area);
+  }
+  return maxArea;
+};
+
+var stockspan = function (stock) {
+  let n = stock.length;
   let s = [];
-  let ans=new Array(n).fill(1);
-  for(let i=0; i<n; i++){
-     while(s.length && stock[s[s.length -1 ]] <= stock[i]){
-       s.pop();
-     }
-     ans[i]= s.length ? i - s[s.length -1] : i+ 1
-     s.push(i)
+  let ans = new Array(n).fill(1);
+  for (let i = 0; i < n; i++) {
+    while (s.length && stock[s[s.length - 1]] <= stock[i]) {
+      s.pop();
+    }
+    ans[i] = s.length ? i - s[s.length - 1] : i + 1
+    s.push(i)
   }
 
   return ans;
- } 
+}
 
- console.log("StackSpan===>", stockspan([100 ,80, 60,70,60,75,85]))
+console.log("StackSpan===>", stockspan([100, 80, 60, 70, 60, 75, 85]))
 
 
- var celebritySearch = function(grid) {
+var celebritySearch = function (grid) {
   let n = grid.length;
   let stack = [];
 
@@ -249,7 +249,7 @@ var largestRectangleArea = function(heights) {
 
 console.log(
   "Found celebrity -======>",
-  celebritySearch([[0,1,0],[0,0,0],[0,1,0]])
+  celebritySearch([[0, 1, 0], [0, 0, 0], [0, 1, 0]])
 );
 
 
@@ -265,26 +265,26 @@ var maxSumMinProduct = function (nums) {
   let leftMin = new Array(n).fill(-1);
   let rightMin = new Array(n).fill(n);
   for (let i = 0; i < n; i++) {
-      prefixSum[i] = i == 0 ? nums[i] : prefixSum[i - 1] + nums[i];
-      while (stack1.length && nums[stack1[stack1.length - 1]] >= nums[i]) {
-          stack1.pop();
-      }
-      leftMin[i] = stack1.length !== 0 ? stack1[stack1.length - 1] : -1;
-      stack1.push(i);
-      while (stack2.length && nums[stack2[stack2.length - 1]] > nums[i]) {
-          let idx = stack2.pop();
-          rightMin[idx] = i;
-      }
-      stack2.push(i);
+    prefixSum[i] = i == 0 ? nums[i] : prefixSum[i - 1] + nums[i];
+    while (stack1.length && nums[stack1[stack1.length - 1]] >= nums[i]) {
+      stack1.pop();
+    }
+    leftMin[i] = stack1.length !== 0 ? stack1[stack1.length - 1] : -1;
+    stack1.push(i);
+    while (stack2.length && nums[stack2[stack2.length - 1]] > nums[i]) {
+      let idx = stack2.pop();
+      rightMin[idx] = i;
+    }
+    stack2.push(i);
   }
 
   let max = 0n;
   for (let i = 0; i < n; i++) {
-      let left = leftMin[i] + 1;
-      let right = rightMin[i] - 1;
-      let total = BigInt(prefixSum[right]) - BigInt(left > 0 ? prefixSum[left - 1] : 0)
-      let curr = total * BigInt(nums[i]);
-      max = curr > max ? curr : max;
+    let left = leftMin[i] + 1;
+    let right = rightMin[i] - 1;
+    let total = BigInt(prefixSum[right]) - BigInt(left > 0 ? prefixSum[left - 1] : 0)
+    let curr = total * BigInt(nums[i]);
+    max = curr > max ? curr : max;
   }
   const MOD = 1000000007n;
   return Number(max % MOD);
@@ -294,147 +294,207 @@ var maxSumMinProduct = function (nums) {
 
 
 
-var MinStack = function() {
-  this.stack1=[];
-  this.stack2=[];
-  };
-  
-  /** 
-   * @param {number} val
-   * @return {void}
-   */
-  MinStack.prototype.push = function(val) {
-     this.stack1.push(val);
-     if(this.stack2.length == 0){
-      this.stack2.push(val);
-     }else{
-     if(this.stack2[this.stack2.length -1] >= val){
-      this.stack2.push(val);
-     }
-     }
-  };
-  
-  /**
-   * @return {void}
-   */
-  MinStack.prototype.pop = function() {
-    if(this.stack1.length){
-      var val = this.stack1.pop();
-    }
-    if(val == this.stack2[this.stack2.length -1] ){
-      this.stack2.pop();
-    }
-  };
-  
-  /**
-   * @return {number}
-   */
-  MinStack.prototype.top = function() {
-  if(this.stack1.length){
-     return this.stack1[this.stack1.length -1];
-    }
-    return null;
-  };
-  
-  /**
-   * @return {number}
-   */
-  MinStack.prototype.getMin = function() {
-      if(this.stack2.length){
-     return this.stack2[this.stack2.length -1];
-    }
-    return null;
-  };
-  
-  /** 
-   * Your MinStack object will be instantiated and called as such:
-   * var obj = new MinStack()
-   * obj.push(val)
-   * obj.pop()
-   * var param_3 = obj.top()
-   * var param_4 = obj.getMin()
-   */
+var MinStack = function () {
+  this.stack1 = [];
+  this.stack2 = [];
+};
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  this.stack1.push(val);
+  if (this.stack2.length == 0) {
+    this.stack2.push(val);
+  } else {
+    if (this.stack2[this.stack2.length - 1] >= val) {
+      this.stack2.push(val);
+    }
+  }
+};
 
-  /**
- * @param {number[]} gas
- * @param {number[]} cost
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  if (this.stack1.length) {
+    var val = this.stack1.pop();
+  }
+  if (val == this.stack2[this.stack2.length - 1]) {
+    this.stack2.pop();
+  }
+};
+
+/**
  * @return {number}
  */
-var canCompleteCircuit = function(gas, cost) {
-  let totalGas = 0;
-  let totalCost= 0;
-  
-  for(let i =0 ; i< gas.length ; i++){
-      totalGas += gas[i];
-      totalCost += cost[i];
+MinStack.prototype.top = function () {
+  if (this.stack1.length) {
+    return this.stack1[this.stack1.length - 1];
   }
-  if(totalCost > totalGas) return -1;
-  
-  let curr = 0 ;
-      let start= 0 ;
-  for(let i = 0 ; i<gas.length ; i++){
-      curr += gas[i] - cost[i];
-      if(curr < 0 ){
-          start=i+1;
-  curr = 0 ;
-      } 
+  return null;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  if (this.stack2.length) {
+    return this.stack2[this.stack2.length - 1];
+  }
+  return null;
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+
+/**
+* @param {number[]} gas
+* @param {number[]} cost
+* @return {number}
+*/
+var canCompleteCircuit = function (gas, cost) {
+  let totalGas = 0;
+  let totalCost = 0;
+
+  for (let i = 0; i < gas.length; i++) {
+    totalGas += gas[i];
+    totalCost += cost[i];
+  }
+  if (totalCost > totalGas) return -1;
+
+  let curr = 0;
+  let start = 0;
+  for (let i = 0; i < gas.length; i++) {
+    curr += gas[i] - cost[i];
+    if (curr < 0) {
+      start = i + 1;
+      curr = 0;
+    }
   }
   return start
-  };
+};
 
 
-  /**
- * @param {number[]} heights
- * @return {number}
- */
-var largestRectangleArea = function(heights) {
-  let n =heights.length;
-  let left=new Array(n).fill(-1);
-  let stack1 =[]
+/**
+* @param {number[]} heights
+* @return {number}
+*/
+var largestRectangleArea = function (heights) {
+  let n = heights.length;
+  let left = new Array(n).fill(-1);
+  let stack1 = []
   let right = new Array(n).fill(n);
-  let stack2=[];
- 
- for(let i=0 ; i<heights.length; i++){
- while(stack1.length && heights[stack1[stack1.length -1]] > heights[i]){
-     stack1.pop();
- }
- left[i]= stack1.length ? stack1[stack1.length -1] : -1;
- stack1.push(i);
- 
- while(stack2.length && heights[stack2[stack2.length -1]] > heights[i]){
-     const insert = stack2.pop();
-     right[insert]= i
- }
- stack2.push(i);
- }
- let maxArea= 0 ;
- for(let i= 0 ; i<heights.length ; i++){
-     let l= left[i] + 1;
-     let r= right[i];
-     let area= heights[i] * (r-l)
-     maxArea= Math.max(maxArea , area);
- }
- return maxArea;
- };
+  let stack2 = [];
 
-
-
-
- var stockspan = function(stock) {
-  let n = stock.length;
-  let stack =[];
-  let ans= new Array(n).fill(1)
-  for(let i=0;i<n;i++){
-    while(stack.length && stock[stack[stack.length-1]] <= stock[i]){
-stack.pop();
+  for (let i = 0; i < heights.length; i++) {
+    while (stack1.length && heights[stack1[stack1.length - 1]] > heights[i]) {
+      stack1.pop();
     }
-if(stack.length){
-  ans[i]= i - stack[stack.length-1] 
-}
-stack.push(i);
+    left[i] = stack1.length ? stack1[stack1.length - 1] : -1;
+    stack1.push(i);
+
+    while (stack2.length && heights[stack2[stack2.length - 1]] > heights[i]) {
+      const insert = stack2.pop();
+      right[insert] = i
+    }
+    stack2.push(i);
+  }
+  let maxArea = 0;
+  for (let i = 0; i < heights.length; i++) {
+    let l = left[i] + 1;
+    let r = right[i];
+    let area = heights[i] * (r - l)
+    maxArea = Math.max(maxArea, area);
+  }
+  return maxArea;
+};
+
+
+
+
+var stockspan = function (stock) {
+  let n = stock.length;
+  let stack = [];
+  let ans = new Array(n).fill(1)
+  for (let i = 0; i < n; i++) {
+    while (stack.length && stock[stack[stack.length - 1]] <= stock[i]) {
+      stack.pop();
+    }
+    if (stack.length) {
+      ans[i] = i - stack[stack.length - 1]
+    }
+    stack.push(i);
   }
   return ans
- } 
+}
 
- console.log("StackSpan===>", stockspan([100 ,80, 60,70,60,75,85]))
+console.log("StackSpan===>", stockspan([100, 80, 60, 70, 60, 75, 85]))
+
+//  Celebrity Problem
+// You are given an N × N matrix M representing N people at a party.
+// M[i][j] = 1 means person i knows person j.
+// M[i][j] = 0 means person i does not know person j.
+// A celebrity is a person who:
+// Is known by everyone else.
+// Knows no one else.
+// Find the index of the celebrity if one exists; otherwise return -1.
+// Example:
+// Input:
+// M = [
+//  [0,1,0],
+//  [0,0,0],
+//  [0,1,0]
+// ]
+// Output:
+// 1
+// Because person 1 is known by everyone and knows nobody.
+
+var celebritySearch = function (grid) {
+  let n = grid.length;
+  let top = 0;
+  let down = n - 1;
+  while (top < down) {
+    if (grid[top][down] == 1) {
+      top++;          // top can't be celebrity
+    }
+    else if (grid[down][top] == 1) {
+      down--;         // down can't be celebrity
+    }
+    else {
+      // neither knows the other
+      // both can't be celebrity
+      top++;
+      down--;
+    }
+  }
+
+  if (top > down) return -1;
+
+  if (top == down) {
+    for (let i = 0; i < n; i++) {
+      if (top == i) {
+        continue;
+      }
+      if (grid[top][i] == 0 && grid[i][top] == 1) {
+        continue;
+      } else {
+        return -1
+      }
+    }
+    return top;
+  }
+};
+
+console.log(
+  "Found celebrity -======>",
+  celebritySearch([[0, 1, 1, 0], [0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0]])
+);
